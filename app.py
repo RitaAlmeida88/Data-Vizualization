1c86fea6650ac267d1335963e41520ac455a4adb
RitaAlmeida88 <135462594+RitaAlmeida88@users.noreply.github.com>
2023-06-04 00:01:10 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import pandas as pd
+import plotly.express as px
 import plotly.graph_objs as go
-
 
 df = pd.read_csv('accidents2016.csv')
 
